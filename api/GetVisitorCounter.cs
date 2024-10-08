8733d7b8b2dfa63fd65022711ed7b747db85b229
heyitschongchin <chong-chin@hotmail.com>
2024-10-08 22:26:30 +0800

    Updated with .Net 8.0 + dotnet add package Microsoft.Azure.Functions.Worker.Extensions.CosmosDB --version 4.11.0

---
+++ api/GetVisitorCounter.cs
@@ -0,0 +1,65 @@
+using System.Collections.Generic;
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace Api.Function
+{
+    public class GetVisitorCounter
+    {
+        private readonly ILogger<GetVisitorCounter> _logger;
+
+        public GetVisitorCounter(ILogger<GetVisitorCounter> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("GetVisitorCounter")]
+        [CosmosDBOutput("AzureResume", "Counter", Connection = "CosmosDbConnectionString")]
+        public async Task<Counter> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+            [CosmosDBInput("AzureResume", "Counter", Connection = "CosmosDbConnectionString", Id = "1", PartitionKey = "1")] Counter counter)
+        {
+            _logger.LogInformation("Processing request...");
+
+            counter.Count++;
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "application/json; charset=utf-8");
+            var jsonString = JsonSerializer.Serialize(new { id = counter.Id, count = counter.Count });
+            await response.WriteStringAsync(jsonString);
+
+            return counter; // This is what will be saved back to Cosmos DB
+        }
+    }
+
+    public class Counter
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("count")]
+        public int Count { get; set; }
+    }
+}
+/*
+namespace Api.Function
+{
+    public static class GetVisitorCounter
+    {
+        [Function("GetVisitorCounter")]
+        [CosmosDBOutput("AzureResume", "Counter", Connection = "CosmosDBConnectionString")]
+        public static object Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+        [CosmosDBInput("AzureResume", "Counter", Connection = "CosmosDBConnectionString", Id = "1", PartitionKey = "1")] Counter counter,
+            FunctionContext executionContext)
+        {
+            counter.Count++;
+            return counter;
+        }
+    }
+
+}
+*/
